316b10f27a64218ae4633965660d4f74996ef9fc
huu-bo <huubhoogweg@gmail.com>
2024-03-21 20:52:59 +0100

    added median to benchmark.py

--- benchmark.py
+++ benchmark.py
@@ -3,6 +3,7 @@ from time import time
 from wordle import Wordle
 from bot import WordleBot
 
+SCORE_MAX = 20
 
 time_start = time()
 performance = {}
@@ -21,12 +22,30 @@ for i in range(100):
 print(performance)
 time_took = time() - time_start
 
-for i in range(20):
+median = 0
+total_frequency = 0
+for i in range(SCORE_MAX):
+    score = performance.get(i, 0)
+    total_frequency += score
+frequency = 0
+for i in range(SCORE_MAX):
+    score = performance.get(i, 0)
+    frequency += score
+    if frequency > total_frequency // 2:
+        median = i
+        break
+print('median:', median)
+
+for i in range(SCORE_MAX):
     if i > 6:
         print('\033[43m', end='')
+    if i == median:
+        print('\033[92m', end='')
 
     score = performance.get(i, 0)
     print(str(i).rjust(4, ' ') + '|' + '#' * score)
+
+    print('\033[0m', end='')
 
 print('\033[0m', end='')
 print('time took:', time_took, 'seconds')
