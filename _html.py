4e952a7ffa5dfec5f90f75ea6b523def55595b3f
huu-bo <huubhoogweg@gmail.com>
2024-09-01 11:55:48 +0200

    Optimal second word html

---
+++ _html.py
@@ -0,0 +1,62 @@
+import io
+
+
+def word_group(start_word: str, state: str, followup_word: str) -> str:
+    colour = {
+        'B': 'black',
+        'Y': 'yellow',
+        'G': 'green'
+    }
+
+    out = ''
+    out += '<div class="word-group">'
+    out += '<div class="word">'
+    for i in range(len(start_word)):
+        out += f'<div class="letter letter-{colour[state[i]]}">{start_word[i]}</div>'
+    out += '</div>'
+
+    out += '<div class="word">'
+    for i in range(len(followup_word)):
+        out += f'<div class="letter letter-none">{followup_word[i]}</div>'
+    out += '</div>'
+    out += '</div>\n'
+
+    return out
+
+
+with open('test.html', 'r') as file:
+    template = file.read()
+
+
+def sort_func(content) -> int:
+    _, state, _ = content
+    state: str
+    assert len(state) == 5
+
+    out = 0
+    # out |= state.count('G') << 13
+    out |= (state.count('Y') + state.count('G')) << 10
+    out |= int(state.replace('G', '10').replace('Y', '01').replace('B', '00'), base=2)
+    return out
+
+
+contents = []
+body = io.StringIO()
+with open('tarse.tree', 'r', encoding='ascii') as tree:
+    word = tree.read(6)[:-1]
+    # ⚠️ ⚠️ WARNING bad code alert! ⚠️ ⚠️
+    while len(line := tree.readline()) == 16:
+        index = tree.tell()
+        state = line[:5]
+        tree.seek(int(line[5:-1]))
+        # body.write(word_group(word, state, tree.read(5)))
+        contents.append((word, state, tree.read(5)))
+        tree.seek(index)
+
+contents.sort(key=sort_func)
+# print([c[1] for c in contents])
+for content in contents:
+    body.write(word_group(*content))
+
+with open('out.html', 'w') as file:
+    file.write(template.replace('{{BODY}}', body.getvalue()))
